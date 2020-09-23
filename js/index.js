fd806ecf371537b4358da4dc3f296f6afb594dcd
Ifenkiul <61749481+Ifenkiul@users.noreply.github.com>
2020-09-23 22:31:46 +0300

    fixes

--- js/index.js
+++ js/index.js
@@ -1,60 +1,45 @@
-let visible = true;
-//--------------------------------------SIDEBAR SLIDING
-document.querySelector(".nav__sidebar").addEventListener("click", () => {
-  if (visible) {
-    document.querySelector(".grid__container").style.gridTemplateAreas =
-      '"header header header header""main main main main"';
-    document.querySelector(".side").style.display = "none";
-    document.querySelector(".nav__sidebar").innerHTML = "&#9658;";
-    visible = false;
-  } else {
-    document.querySelector(".grid__container").style.gridTemplateAreas =
-      '"header header header header""side main main main"';
-    document.querySelector(".side").style.display = "unset";
-    document.querySelector(".nav__sidebar").innerHTML = "&#9668;";
-    visible = true;
-  }
-});
-
-//----------------------------------LOADING HTML TO MAIN BLOCK
+//+++++++++++++++++++++++++++++++++++++++++ DYNAMIC LOAD OF PAGE TO MAIN BLOCK
 async function loadHtml(link) {
   const response = await fetch(link);
   const html = await response.text();
-  document.querySelector(".main").innerHTML =
-    "<span class='nav__sidebar'>&#9668;</span>" + html;
 
-  document.querySelector(".nav__sidebar").addEventListener("click", () => {
-    if (visible) {
-      document.querySelector(".grid__container").style.gridTemplateAreas =
-        '"header header header header""main main main main"';
-      document.querySelector(".side").style.display = "none";
-      document.querySelector(".nav__sidebar").innerHTML = "&#9658;";
-      visible = false;
-    } else {
-      document.querySelector(".grid__container").style.gridTemplateAreas =
-        '"header header header header""side main main main"';
-      document.querySelector(".side").style.display = "unset";
-      document.querySelector(".nav__sidebar").innerHTML = "&#9668;";
-      visible = true;
-    }
-  });
-
+  document.querySelector(".main_container").innerHTML = html;
   document.querySelectorAll(".main__link--item").forEach((element) =>
     element.addEventListener("click", (event) => {
-      if (event.currentTarget.dataset.clicked === "false") {
-        event.currentTarget.dataset.clicked = "true";
-        event.currentTarget.nextElementSibling.style = "display:unset";
+      const elementClicked = event.currentTarget;
+      if (elementClicked.nextElementSibling.hidden) {
+        elementClicked.nextElementSibling.hidden = false;
       } else {
-        event.currentTarget.dataset.clicked = "false";
-        event.currentTarget.nextElementSibling.style = "display:none";
+        elementClicked.nextElementSibling.hidden = true;
       }
     })
   );
 }
+
+//++++++++++++++++++++++++++++++++++++ SIDEBAR HIDING
+document.querySelector(".btn__close--sidebar").addEventListener("click", () => {
+  const sideMenu = document.querySelector("sidebar");
+  if (sideMenu.hidden) {
+    sideMenu.hidden = false;
+    document.querySelector(".grid__container").style.gridTemplateAreas = "";
+    document.querySelector(".btn__close--sidebar").innerHTML = "&#8678;";
+  } else {
+    sideMenu.hidden = true;
+    document.querySelector(".grid__container").style.gridTemplateAreas =
+      '"header header header header""main main main main"';
+    document.querySelector(".btn__close--sidebar").innerHTML = "&#8680;";
+  }
+});
+
+//********************************************************* SIDEBAR MENU ELEMENT CLICK
+let previousSideClicked = null;
 document.querySelectorAll(".sidebar__menu--item").forEach((element) => {
   element.addEventListener("click", (event) => {
+    if (previousSideClicked) {
+      previousSideClicked.style.color = "";
+    }
+    previousSideClicked = event.currentTarget;
+    previousSideClicked.style.color = "yellowgreen";
     loadHtml(event.currentTarget.dataset.url);
   });
 });
-
-//------------------------------------NAVIGATION THROUGH LINKS
